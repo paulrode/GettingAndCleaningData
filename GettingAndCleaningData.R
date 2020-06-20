ea340a8f0c1908119d841d73f7ee14d429ee6a9e
paulrode <paulrode@outlook.com>
2020-06-20 12:10:00 -0400

    Script setup

---
+++ GettingAndCleaningData.R
@@ -0,0 +1,25 @@
+
+
+# Getting and Cleaning Data
+
+# Week 1, question 1
+# Downloading Files 
+# File to retreve from the internet: https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv
+
+# Set proper working Dir
+setwd("./GettingAndCleaningData")
+getwd()
+
+# Check for data directory and if one is not present then make it
+if (!file.exists("data")) {
+  dir.create("data")
+}
+
+# Download data using a URL into th data directory
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileUrl, destfile = "./data/Fss06hid.csv", method = "curl")
+dateDownloaded <- date()
+dateDownloaded
+list.files("./data")
+
+# Look at the data 

a2e31e6b3a926d31241dd986ffa76cba9bab89e4
paulrode <paulrode@outlook.com>
2020-06-20 13:06:13 -0400

    Worked on getting file in proper format

--- GettingAndCleaningData.R
+++ GettingAndCleaningData.R
@@ -1,10 +1,17 @@
-
-
 # Getting and Cleaning Data
 
 # Week 1, question 1
 # Downloading Files 
 # File to retreve from the internet: https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv
+
+# Load my standard package set
+library("tidyverse")
+library("lubridate")
+library("knitr")
+library("kableExtra")
+library("fpp2")
+library("readxl")
+
 
 # Set proper working Dir
 setwd("./GettingAndCleaningData")
@@ -17,9 +24,19 @@ if (!file.exists("data")) {
 
 # Download data using a URL into th data directory
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-download.file(fileUrl, destfile = "./data/Fss06hid.csv", method = "curl")
+download.file(fileUrl, destfile = "./data/AmericanConsumer")
 dateDownloaded <- date()
 dateDownloaded
 list.files("./data")
 
 # Look at the data 
+AmericanConsumer <- data.frame("./data/AmericanConsumer")
+glimpse("./data/AmericanConsumer")
+
+
+
+setwd("./data")
+getwd()
+list.files()
+glimpse("AmericanConsumer")
+class("AmericanConsumer")

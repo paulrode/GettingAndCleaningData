719544a95731a8ba3b6e996ee08369aa9bc407d4
paulrode <paulrode@outlook.com>
2020-06-26 22:12:55 -0400

    finsihed quiz

--- GettingAndCleaningData.R
+++ GettingAndCleaningData.R
@@ -4,17 +4,26 @@
 # Downloading Files 
 # File to retreve from the internet: https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv
 
-# Load my standard package set
+# Package for tidyverse 
 library("tidyverse")
 library("lubridate")
+# Package for building tables in markdown and notebook 
 library("knitr")
 library("kableExtra")
+# Package for forecasting
 library("fpp2")
+# Packages for reading excel and html files and XML
 library("openxlsx")
 library("XML")
+# Parkage for using data tables for very large data operations
 library("data.table")
+#Package for reading fixed width tables
+library("utils")
+# Packages for reading data through API's 
 library("httr")
 library("jsonlite")
+# Package for performing inquires with SQL databases 
+library("sqldf")
 
 
 # Set proper working Dir
@@ -162,3 +171,76 @@ json2[ , c(3 == "datasharing",47)]
 json2 %>% filter(name == "datasharing") %>% select(name, created_at)
 
 
+# Question 2
+# The sqldf package allows for execution of SQL commands on R data 
+# frames. We will use the sqldf package to practice the queries we 
+# might send with the dbSendQuery command in RMySQL.
+# Download Download the American Community Survey data and load it 
+# into an R object called acs. https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv
+
+# Set proper working Dir
+if (!getwd() == "C:/Users/paulr/Documents/R/GettingAndCleaningData") {setwd("./GettingAndCleaningData")}
+getwd()
+# Check for data directory and if one is not present then make it
+if (!file.exists("data")) {
+  dir.create("data")}
+# Get file
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrl, destfile = "./data/Fss06pid.csv")
+dateDownloaded <- date()
+acs <- read.csv("./data/Fss06pid.csv")
+glimpse(acs)
+str(sqldf("select pwgtp1 from acs where AGEP<50"))
+
+# Question 3
+# Which of the following commands will select only the data for the probability weights pwgtp1 with 
+# ages less than 50?
+unique(acs$AGEP)
+glimpse(unique(acs$AGEP))
+sqldf("select AGEP where unique from acs") #no
+sqldf("select unique AGEP from ACS") #no
+sqldf("select distinct AGEP from acs") # answer
+sqldf("select distinct pwgtp1 from acs") #no
+
+
+# Question 4 Webscraping 
+# Which of the following commands will select only the data for 
+# the probability weights pwgtp1 with ages less than 50?
+# http://biostat.jhsph.edu/~jleek/contact.html
+
+# Set proper working Dir
+if (!getwd() == "C:/Users/paulr/Documents/R/GettingAndCleaningData") {setwd("./GettingAndCleaningData")}
+getwd()
+# Check for data directory and if one is not present then make it
+if (!file.exists("data")) {
+  dir.create("data")}
+# Get Page
+con = url("http://biostat.jhsph.edu/~jleek/contact.html")
+htmlCode = readLines(con)
+close(con)
+htmlCode
+nchar(htmlCode[c(10, 20, 30, 100)])
+
+
+#Question 5 
+# Read this data set into R and report the sum of the numbers in 
+# the fourth of the nine columns.
+# https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for
+# Original source of the data: http://www.cpc.ncep.noaa.gov/data/indices/wksst8110.for
+# (Hint this is a fixed width file format)
+
+# Set proper working Dir
+if (!getwd() == "C:/Users/paulr/Documents/R/GettingAndCleaningData") {setwd("./GettingAndCleaningData")}
+getwd()
+# Check for data directory and if one is not present then make it
+if (!file.exists("data")) {
+  dir.create("data")}
+# Get Page
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+download.file(fileUrl, destfile = "./data/Fwksst8110.for")
+dateDownloaded <- date()
+#Use the read.table() funciton
+# con <- read.table("./data/Fwksst8110.for", skip = 4, header = TRUE)
+con <- read.fwf("./data/Fwksst8110.for", widths = c(-1, 9, -5, 4,-1,3, -5, 4,-1,3, -5, 4,-1,3, -5, 4,-1,3), col.names = c("week", "col1", "col2", "col3", "col4", "col5", "col6", "col7","col8"), strip.white = TRUE, skip = 4)
+con
+sum(as.numeric(con$col3))
